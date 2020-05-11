2d863911214b61efe4efdc9d49a1d51558185c41
1910004 <1910004@1605008nb50.iead.local>
2020-05-11 14:36:42 +0800

    MODIFY: autowired setter method

--- spring-demo-annotation/src/com/asnif/springdemo/TennisCoach.java
+++ spring-demo-annotation/src/com/asnif/springdemo/TennisCoach.java
@@ -9,14 +9,26 @@ public class TennisCoach implements Coach {
 
 	private FortuneService fortuneService;
 	
+	// define a default constructor
+	public TennisCoach() {
+		System.out.println(">> TennisCoach: inside default constructor");
+	}
+	
+	// define a setter method
 	@Autowired
-	public TennisCoach(FortuneService theFortuneService) {
+	public void setFortuneService(FortuneService theFortuneService) {
+		System.out.println(">> TennisCoach: inside setFortuneService() method");
 		fortuneService = theFortuneService;
 	}
 	
+//	@Autowired
+//	public TennisCoach(FortuneService theFortuneService) {
+//		fortuneService = theFortuneService;
+//	}
+	
 	@Override
 	public String getDailyWorkout() {
-		return "Practice your backhand volley";
+		return "Practice your backhand volley!!";
 	}
 
 	@Override
