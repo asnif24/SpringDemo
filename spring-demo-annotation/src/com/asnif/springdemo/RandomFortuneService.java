b3576533c543657cd1dd72c9d1ee866a6c9b5bca
1910004 <1910004@1605008nb50.iead.local>
2020-05-11 17:08:32 +0800

    MODIFY: random fortune service

--- spring-demo-annotation/src/com/asnif/springdemo/RandomFortuneService.java
+++ spring-demo-annotation/src/com/asnif/springdemo/RandomFortuneService.java
@@ -1,14 +1,32 @@
 package com.asnif.springdemo;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class RandomFortuneService implements FortuneService {
 
+	// create an array of strings
+	private String[] data = {
+		"happy day",
+		"very happy day",
+		"miao miao miao ~~"
+	};
+	
+	// create random number generator
+	private Random myRandom = new Random();
+	
 	@Override
 	public String getFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		// pick a random string from array
+		int index = myRandom.nextInt(data.length);
+		String theFortune = data[index];
+		return theFortune;
 	}
 
 }
+
+
+
+
