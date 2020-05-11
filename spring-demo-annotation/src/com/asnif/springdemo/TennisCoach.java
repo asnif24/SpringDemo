3743b0b0337de369e7eb389743f0f2bdb4e6bd6d
1910004 <1910004@1910004NB51.iead.local>
2020-05-11 10:20:02 +0800

    ADD: annotation demo app

--- spring-demo-annotation/src/com/asnif/springdemo/TennisCoach.java
+++ spring-demo-annotation/src/com/asnif/springdemo/TennisCoach.java
@@ -1,11 +1,13 @@
 package com.asnif.springdemo;
 
+import org.springframework.stereotype.Component;
+
+@Component("thatSillyCoach")
 public class TennisCoach implements Coach {
 
 	@Override
 	public String getDailyWorkout() {
-		// TODO Auto-generated method stub
-		return null;
+		return "Practice your backhand volley";
 	}
 
 }
