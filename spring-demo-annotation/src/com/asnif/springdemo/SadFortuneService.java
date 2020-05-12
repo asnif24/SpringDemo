56f908b45df51426db4a5d5be6f776e8a7fc7583
1910004 <1910004@1605008nb50.iead.local>
2020-05-12 15:52:54 +0800

    ADD: for java config

---
+++ spring-demo-annotation/src/com/asnif/springdemo/SadFortuneService.java
@@ -0,0 +1,10 @@
+package com.asnif.springdemo;
+
+public class SadFortuneService implements FortuneService {
+
+	@Override
+	public String getFortune() {
+		return "Q_Q";
+	}
+
+}
--- spring-demo-annotation/src/com/asnif/springdemo/SportConfig.java
+++ spring-demo-annotation/src/com/asnif/springdemo/SportConfig.java
@@ -1,10 +1,21 @@
 package com.asnif.springdemo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.asnif.springdemo")
+//@ComponentScan("com.asnif.springdemo")
 public class SportConfig {
-
+	// define bean for our SadFortuneService 
+	@Bean
+	public FortuneService sadFortuneService() {
+		return new SadFortuneService();
+	}
+	
+	// define bean for our SwimCoach and inject dependency
+	@Bean
+	public Coach swimCoach() {
+		return new SwimCoach(sadFortuneService());
+	}
 }
