2680f2155e0248e97dba156ce9ca7e39fc0982df
1910004 <1910004@1605008nb50.iead.local>
2020-05-12 12:40:33 +0800

    ADD: for java config demo

---
+++ spring-demo-annotation/src/com/asnif/springdemo/SportConfig.java
@@ -0,0 +1,10 @@
+package com.asnif.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.asnif.springdemo")
+public class SportConfig {
+
+}
