9cda4a2f7b2c66dd088a024d62c005f85e2d2ae2
1910004 <1910004@1605008nb50.iead.local>
2020-05-12 18:04:14 +0800

    ADD: inject value from property file

--- spring-demo-annotation/src/com/asnif/springdemo/SportConfig.java
+++ spring-demo-annotation/src/com/asnif/springdemo/SportConfig.java
@@ -3,9 +3,11 @@ package com.asnif.springdemo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 //@ComponentScan("com.asnif.springdemo")
+@PropertySource("classpath:sport.properties")
 public class SportConfig {
 	// define bean for our SadFortuneService 
 	@Bean
--- spring-demo-annotation/src/com/asnif/springdemo/SwimCoach.java
+++ spring-demo-annotation/src/com/asnif/springdemo/SwimCoach.java
@@ -1,9 +1,18 @@
 package com.asnif.springdemo;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class SwimCoach implements Coach {
 
 	private FortuneService fortuneService;
 	
+	@Value("${foo.email}")
+	private String email;
+	
+	@Value("${foo.team}")
+	private String team;
+	
+
 	public SwimCoach(FortuneService theFortuneService) {
 		fortuneService = theFortuneService;
 	}
@@ -16,6 +25,14 @@ public class SwimCoach implements Coach {
 	@Override
 	public String getDailyFortune() {
 		return fortuneService.getFortune();
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getTeam() {
+		return team;
 	}
 
 }
