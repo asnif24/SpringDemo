63a114d92557d184dd5a7dd417b2ebf5dc00c2e5
1910004 <1910004@192.168.56.1>
2020-05-13 18:02:18 +0800

    ADD: Home Page

---
+++ spring-mvc-demo/src/com/asnif/springdemo/mvc/HomeController.java
@@ -0,0 +1,13 @@
+package com.asnif.springdemo.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String showPage() {
+		return null;
+	}
+}
