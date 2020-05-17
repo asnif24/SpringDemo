2c547ef1d36d3f49e863c5d83a4930a930cca3ac
htlin <htlin@192.168.0.104>
2020-05-18 00:19:44 +0800

    ADD: toUpperCase Controller

--- spring-mvc-demo/src/com/asnif/springdemo/mvc/HelloWorldController.java
+++ spring-mvc-demo/src/com/asnif/springdemo/mvc/HelloWorldController.java
@@ -1,6 +1,9 @@
 package com.asnif.springdemo.mvc;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,4 +22,29 @@ public class HelloWorldController {
 		return "helloworld";
 	}
 	
+	// new a controller method to read form data and add data to the model
+	@RequestMapping("/processFormVertionTwo")
+	public String letsShoutDude(HttpServletRequest request, Model model) {
+		// read the request parameter from the HTML form
+		String theName = request.getParameter("studentName");
+		
+		// convert the data to all caps
+		theName = theName.toUpperCase();
+		
+		// create the message 
+		String result = "Yo! " + theName;
+		
+		// add message to the model
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+	}
+	
 }
+
+
+
+
+
+
+
