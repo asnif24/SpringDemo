d4f20ffb4ffdee4003c4818c969f54f536344a43
asnif24 <asnif24@gmail.com>
2020-06-01 11:14:11 +0800

    ADD: instructor detail

---
+++ hibernate-tutorial/src/com/asnif/hibernate/demo/entity/InstructorDetail.java
@@ -0,0 +1,17 @@
+package com.asnif.hibernate.demo.entity;
+
+public class InstructorDetail {
+	
+	// annotate the class as an entity and map to db table
+	
+	// define the field
+	
+	// annotate the fields with db column names
+	
+	// create constructors
+	
+	// generate getter/setter methods
+	
+	// generate toString method
+
+}
