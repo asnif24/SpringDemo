b547cc3de50d47ca54dd1cba366be0eac70cb865
asnif24 <asnif24@gmail.com>
2020-06-11 17:31:34 +0800

    ADD: eager lazy

---
+++ hb-eager-vs-lazy-demo/src/com/asnif/hibernate/demo/EagerLazyDemo.java
@@ -0,0 +1,55 @@
+package com.asnif.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.asnif.hibernate.demo.entity.Course;
+import com.asnif.hibernate.demo.entity.Instructor;
+import com.asnif.hibernate.demo.entity.InstructorDetail;
+
+public class EagerLazyDemo {
+
+	public static void main(String[] args) {
+		// Create session factory
+		SessionFactory factory = new Configuration()
+					.configure("hibernate.cfg.xml")
+					.addAnnotatedClass(Instructor.class)
+					.addAnnotatedClass(InstructorDetail.class)
+					.addAnnotatedClass(Course.class)
+					.buildSessionFactory();
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			
+			// start a transaction
+			session.beginTransaction();
+
+			// get the instructor form DB
+			int theId = 1;
+			Instructor tempInstructor = session.get(Instructor.class, theId);
+			
+			System.out.println("asnif: Instructor: "+ tempInstructor);
+			
+			// get course for the instructor
+			System.out.println("asnif: Courses: "+tempInstructor.getCourses());
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("asnif: Done!!");
+		}
+		finally {
+			session.close();
+			factory.close();
+		}
+		
+	}
+
+}
+
+
+
+
+
