772ae1f631031d5641091b2cc7474d182e9d6611
asnif24 <asnif24@gmail.com>
2020-06-13 21:32:14 +0800

    MODIFY: create course review

--- hb-04-one-to-many-uni/src/com/asnif/hibernate/demo/CreateCourseAndReviewsDemo.java
+++ hb-04-one-to-many-uni/src/com/asnif/hibernate/demo/CreateCourseAndReviewsDemo.java
@@ -7,6 +7,7 @@ import org.hibernate.cfg.Configuration;
 import com.asnif.hibernate.demo.entity.Course;
 import com.asnif.hibernate.demo.entity.Instructor;
 import com.asnif.hibernate.demo.entity.InstructorDetail;
+import com.asnif.hibernate.demo.entity.Review;
 
 public class CreateCourseAndReviewsDemo {
 
@@ -17,6 +18,7 @@ public class CreateCourseAndReviewsDemo {
 					.addAnnotatedClass(Instructor.class)
 					.addAnnotatedClass(InstructorDetail.class)
 					.addAnnotatedClass(Course.class)
+					.addAnnotatedClass(Review.class)
 					.buildSessionFactory();
 		// create session
 		Session session = factory.getCurrentSession();
@@ -27,10 +29,17 @@ public class CreateCourseAndReviewsDemo {
 			session.beginTransaction();
 
 			// create the course
-			Course tempCourse = new Course("");
+			Course tempCourse = new Course("FishBurger - A Great Tuna!!");
 			// add some reviews
+			tempCourse.addReview(new Review("Fish is delicious ><>"));
+			tempCourse.addReview(new Review("Fish is wonderful ><>"));
+			tempCourse.addReview(new Review("Fish is fucking great ><>"));
 			
 			// save the course ... and leverage the cascade all :)
+			System.out.println("Saving the course: ");
+			System.out.println(tempCourse);
+			System.out.println(tempCourse.getReviews());
+			session.save(tempCourse);
 			
 			// commit transaction
 			session.getTransaction().commit();
