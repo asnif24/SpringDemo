42a01ea4fc7eb09cb250fa9724565b6f9898c9c6
asnif24 <asnif24@gmail.com>
2020-09-24 11:46:06 +0800

    ADD: CSS

--- web-customer-tracker/src/com/asnif/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/com/asnif/springdemo/controller/CustomerController.java
@@ -1,16 +1,32 @@
 package com.asnif.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.asnif.springdemo.dao.CustomerDAO;
+import com.asnif.springdemo.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+	// need to inject the customer DAO
+	@Autowired
+	private CustomerDAO customerDAO;
+	
 	@RequestMapping("/list")
 	public String listCustomers(Model theModel) {
-	
+		
+		// get the customers from the dao
+		List<Customer> theCustomers = customerDAO.getCustomers();
+		
+		// add the customers to the model
+		theModel.addAttribute("customers", theCustomers);
+		
 		return "list-customers";
 	}
 	
---
+++ web-customer-tracker/src/com/asnif/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,11 @@
+package com.asnif.springdemo.dao;
+
+import java.util.List;
+
+import com.asnif.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+	
+}
